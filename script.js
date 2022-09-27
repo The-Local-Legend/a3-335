a5cb99c969b77e8a30f1da9dad4cca054afaca5b
The-Local-Legend <69749174+The-Local-Legend@users.noreply.github.com>
2022-09-27 22:51:57 +1300

    buttons working

--- script.js
+++ script.js
@@ -10,7 +10,53 @@ const getVersion = () => {
     let ver = document.getElementById("Version");
     streamPromise.then((resp) => ver.innerHTML = resp);
     
-    console.log(ver.innerHTML);
+    
     
 }
 getVersion();
+let loggedin = false;
+function move(){
+    document.getElementById("home").style.display = "none";
+    document.getElementById("shop").style.display = "none";
+    document.getElementById("rego").style.display = "none";
+    document.getElementById("login").style.display = "none";
+    document.getElementById("play").style.display = "none";
+    document.getElementById("guest").style.display = "none";
+    document.getElementById("homebtn").style.backgroundColor = "rgb(172, 123, 68)";
+    document.getElementById("shopbtn").style.backgroundColor = "rgb(172, 123, 68)";
+    document.getElementById("regobtn").style.backgroundColor = "rgb(172, 123, 68)";
+    document.getElementById("loginbtn").style.backgroundColor = "rgb(172, 123, 68)";
+    document.getElementById("playbtn").style.backgroundColor = "rgb(172, 123, 68)";
+    document.getElementById("guestbtn").style.backgroundColor = "rgb(172, 123, 68)";
+}
+function home(){
+    move();
+    document.getElementById("home").style.display = "block";
+    document.getElementById("homebtn").style.backgroundColor = "rgb(216, 161, 97)";
+}
+function shop(){
+    move();
+    document.getElementById("shop").style.display = "block";
+    document.getElementById("shopbtn").style.backgroundColor = "rgb(216, 161, 97)";
+}
+function rego(){
+    move();
+    document.getElementById("rego").style.display = "block";
+    document.getElementById("regobtn").style.backgroundColor = "rgb(216, 161, 97)";
+}
+function login(){
+    move();
+    document.getElementById("login").style.display = "block";
+    document.getElementById("loginbtn").style.backgroundColor = "rgb(216, 161, 97)";
+}
+function play(){
+    move();
+    document.getElementById("play").style.display = "block";
+    document.getElementById("playbtn").style.backgroundColor = "rgb(216, 161, 97)";
+}
+function guest(){
+    move();
+    document.getElementById("guest").style.display = "block";
+    document.getElementById("guestbtn").style.backgroundColor = "rgb(216, 161, 97)";
+}
+home();
